4578289660998f9fce15bf4a19d90530635d60ca
Therese Koch <46657075+theresekoch@users.noreply.github.com>
2024-05-04 16:21:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 setup(install_requires=['numpy'], 
-     include - ['emd'])
+     include = ['emd'])
 
 from distutils.core import setup, Extension
 import numpy as np
